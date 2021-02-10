ef7526512ab72ea3ca0e364756f8fdaae98ca553
ANNEX_K3_PRINTER <marc.pot@hotmail.com>
2021-02-10 19:39:29 +0100

    First commit

---
+++ githubupload.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+uploadbool=false
+today=`date +%Y-%m-%d.%H:%M:%S`
+backupdir="backup/"
+
+cd ~/klipper_config
+
+git pull origin master
+
+for file in *[0-9].cfg; do
+        if [ -f "$file" ] # does file exist?
+        then
+		   mv $file $backupdir$file
+		   uploadbool=true
+        fi
+done
+
+git add .
+git commit -m "backup."$today
+git push -u origin master --force
